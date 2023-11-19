ea15c4885400c104eb0b42c82686ee9b9457a685
piyanggoon <piyanggoon.keedmor@gmail.com>
2023-11-19 23:56:41 +0700

    feat: sign, unsign by edge runtime.

--- src/session.ts
+++ src/session.ts
@@ -8,7 +8,6 @@ import {
 } from './types';
 import { nanoid } from 'nanoid';
 import { MemoryStore } from './memory';
-import signature from 'cookie-signature';
 import { cookies } from 'next/headers';
 
 // import { RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies';
@@ -69,30 +68,77 @@ export class AppSession<T extends SessionRecord = SessionRecord>
     return cookies().set(name, value, cookieOpts);
   }
 
-  private _getID(): string | null | undefined {
-    return this.decode(this.getCookie(this.name));
+  private async _getID(): Promise<string | null | undefined> {
+    return await this.decode(this.getCookie(this.name));
   }
 
-  private _initID() {
-    let id = this._getID();
+  private async _initID() {
+    let id = await this._getID();
     if (!id && this.genid) {
       id = this.genid();
     }
     this.sid = id || '';
   }
 
-  private encode(sid: string): string {
-    if (!this.secret || this.secret == '') return sid;
-    return sid ? 's:' + signature.sign(sid, this.secret || '') : '';
+  private async sign(input: string, secret: string): Promise<string | null> {
+    try {
+      const encoder = new TextEncoder();
+      const data = encoder.encode(input);
+      const secretKey = encoder.encode(secret);
+  
+      const subtleCrypto = crypto.subtle;
+      const algorithm = { name: 'HMAC', hash: 'SHA-256' };
+      const cryptoKey = await subtleCrypto.importKey('raw', secretKey, algorithm, false, ['sign']);
+  
+      const signature = await subtleCrypto.sign(algorithm, cryptoKey, data)
+  
+      const signatureArray = Array.from(new Uint8Array(signature));
+      const signatureString = signatureArray.map(byte => String.fromCharCode(byte)).join('');
+      const base64Signature = btoa(signatureString);
+  
+      return input + '.' + base64Signature.replace(/\=+$/, '');
+    } catch {
+      return null;
+    }
   }
 
-  private decode(raw: string | null | undefined): string | null {
+  private async unsign(input: string, secret: string): Promise<string | null> {
+    try {
+      const encoder = new TextEncoder();
+      const inputParts = input.split('.');
+      const val = inputParts[0];
+      const base64Signature = inputParts[1];
+      const signature = atob(base64Signature);
+  
+      const data = encoder.encode(val);
+      const signatureArray = Array.from(signature).map(char => char.charCodeAt(0));
+      const signatureBytes = new Uint8Array(signatureArray);
+  
+      const subtleCrypto = crypto.subtle;
+      const algorithm = { name: 'HMAC', hash: 'SHA-256' };
+      const secretKey = encoder.encode(secret);
+  
+      const cryptoKey = await  subtleCrypto.importKey('raw', secretKey, algorithm, false, ['verify'])
+      const isValid = await subtleCrypto.verify(algorithm, cryptoKey, signatureBytes, data);
+  
+      return isValid ? val : null;
+    } catch {
+      return null;
+    }
+  }
+
+  private async encode(sid: string): Promise<string> {
+    if (!this.secret || this.secret == '') return sid;
+    return sid ? 's:' + await this.sign(sid, this.secret || '') : '';
+  }
+
+  private async decode(raw: string | null | undefined): Promise<string | null> {
     if (!raw || !this.secret || this.secret == '') return raw || null;
-    return signature.unsign(raw.slice(2), this.secret || '') || null;
+    return await this.unsign(raw.slice(2), this.secret || '');
   }
 
   async all(): Promise<SessionData<T> | null | undefined> {
-    this._initID();
+    await this._initID();
     const data = await this.store?.get(this.sid);
     return data ?? {};
   }
@@ -113,8 +159,8 @@ export class AppSession<T extends SessionRecord = SessionRecord>
     await this.setAll(data);
   }
   async setAll(data: T): Promise<void> {
-    this._initID();
-    const existingID = this._getID();
+    await this._initID();
+    const existingID = await this._getID();
     if (!existingID || existingID == '') {
       await this.setCookie(this.name, this.encode(this.sid), {
         path: this.cookieOpts?.path || '/',
